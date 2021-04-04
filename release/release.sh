578ca0f4a44eb385f7022487e5c1bf3e2e95e655
Alejandro Falkowski <alexrfalkowski@gmail.com>
2021-04-04 16:15:41 +0200

    feat(release): add gh release (#23)

--- release/release.sh
+++ release/release.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+# Tag the new release
 standard-version --releaseCommitMessageFormat "chore(release): {{currentTag}} [ci skip]"
 git push --follow-tags origin master
+
+# Create a GH release
+echo "${GITHUB_TOKEN}" | gh auth login --with-token
+latest_tag=$(git tag | sort -V | tail -1)
+gh release create "$latest_tag" -t "$latest_tag"
