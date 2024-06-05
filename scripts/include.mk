708d7cf80a24a577d150a508795b300d8f37edee
Alex Falkowski <alexrfalkowski@gmail.com>
2024-06-05 22:29:42 +0200

    feat(ci): use slim

--- scripts/include.mk
+++ scripts/include.mk
@@ -1,6 +1,7 @@
 # Build docker image.
 build-docker:
 	docker build --compress -t alexfalkowski/$(IMAGE) .
+	slim build --sensor-ipc-mode proxy --sensor-ipc-endpoint 172.17.0.1 --http-probe=false alexfalkowski/$(IMAGE)
 
 # Push built docker image.
 push-docker: build-docker
