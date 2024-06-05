56ecb8f61db7d9be46051199012ffb444f0030b3
Alex Falkowski <alexrfalkowski@gmail.com>
2024-06-05 22:06:22 +0200

    feat(ci): use docker image

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -1,28 +1,11 @@
 #!/usr/bin/env bash
 
-changed() {
-    current_branch=$(git symbolic-ref -q --short HEAD)
-
-    if [ "$current_branch" == "master" ]; then
-        diff=$(git describe --tags --abbrev=0 HEAD)
-    else
-        diff="master"
-    fi
-
-    git diff --quiet HEAD "$diff" -- "$1" || echo changed
-}
-
 perform() {
     # shellcheck disable=SC2038
     dirs=$(find . -mindepth 1 -maxdepth 1 -type d -not -name scripts -not -name bin -not -name grafana -not -name prometheus -not -name vault -not -path '*/\.*' | xargs -n 1 basename)
 
     for dir in $dirs
     do
-        res=$(changed "$dir")
-
-        if [ "$res" == "changed" ]; then
-            echo "$1 $dir"
-            make -C "$dir" "$1"
-        fi
+      make -C "$dir" "$1"
     done
 }
