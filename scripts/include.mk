89eff36fd936be7f75ede082530994a351cc9439
Alex Falkowski <alexrfalkowski@gmail.com>
2024-06-06 07:21:19 +0200

    feat(ci): seperate version so we can push all tags

--- scripts/include.mk
+++ scripts/include.mk
@@ -1,11 +1,11 @@
 # Build docker image.
 build-docker:
-	docker build --compress -t alexfalkowski/$(IMAGE) .
-	slim build --http-probe-off --continue-after=1 --target alexfalkowski/$(IMAGE) --tag alexfalkowski/$(IMAGE)-slim
+	docker build --compress -t alexfalkowski/$(IMAGE):$(VERSION) .
+	slim build --http-probe-off --continue-after=1 --target alexfalkowski/$(IMAGE):$(VERSION) --tag alexfalkowski/$(IMAGE):$(VERSION)-slim
 
 # Push built docker image.
 push-docker: build-docker
-	docker push alexfalkowski/$(IMAGE) alexfalkowski/$(IMAGE)-slim
+	docker push alexfalkowski/$(IMAGE) --all-tags
 
 # Lint docker image.
 lint-docker:
