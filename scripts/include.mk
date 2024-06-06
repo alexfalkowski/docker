4abbb45164a15887a111f6d501522422ef568f2d
Alex Falkowski <alexrfalkowski@gmail.com>
2024-06-06 06:59:10 +0200

    feat(ci): build fat and slim image

--- scripts/include.mk
+++ scripts/include.mk
@@ -1,10 +1,11 @@
 # Build docker image.
 build-docker:
-	slim build --http-probe-off --continue-after=1 --target alexfalkowski/$(IMAGE) .
+	docker build --compress -t alexfalkowski/$(IMAGE) .
+	slim build --http-probe-off --continue-after=1 --target alexfalkowski/$(IMAGE) --tag alexfalkowski/$(IMAGE)-slim
 
 # Push built docker image.
 push-docker: build-docker
-	docker push alexfalkowski/$(IMAGE)
+	docker push alexfalkowski/$(IMAGE) alexfalkowski/$(IMAGE)-slim
 
 # Lint docker image.
 lint-docker:
