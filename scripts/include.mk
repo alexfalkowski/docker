a22b974d1640d7d8727236916821bc0552c11760
Alex Falkowski <alexrfalkowski@gmail.com>
2024-06-06 11:43:16 +0200

    feat(deps): remove slim

--- scripts/include.mk
+++ scripts/include.mk
@@ -1,7 +1,6 @@
 # Build docker image.
 build-docker:
 	docker build -t alexfalkowski/$(IMAGE):$(VERSION) .
-	slim build --preserve-path-file ../scripts/preserved_paths.txt --http-probe-off --continue-after=1 --target alexfalkowski/$(IMAGE):$(VERSION) --tag alexfalkowski/$(IMAGE):$(VERSION)-slim
 
 # Push built docker image.
 push-docker: build-docker
