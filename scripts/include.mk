d1233b8afd5d7e7eb87325ca7b02dc770244db15
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-06-06 10:27:15 +0200

    feat(ci): use preserve-path-file (#405)

--- scripts/include.mk
+++ scripts/include.mk
@@ -1,7 +1,7 @@
 # Build docker image.
 build-docker:
 	docker build -t alexfalkowski/$(IMAGE):$(VERSION) .
-	slim build --http-probe-off --continue-after=1 --target alexfalkowski/$(IMAGE):$(VERSION) --tag alexfalkowski/$(IMAGE):$(VERSION)-slim
+	slim build --preserve-path-file ../scripts/preserved_paths.txt --http-probe-off --continue-after=1 --target alexfalkowski/$(IMAGE):$(VERSION) --tag alexfalkowski/$(IMAGE):$(VERSION)-slim
 
 # Push built docker image.
 push-docker: build-docker
