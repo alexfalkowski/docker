ff6669024ca63eae4aa3e64e569c37d3bcde5cfc
Alex Falkowski <alexrfalkowski@gmail.com>
2025-03-01 11:53:47 +0100

    feat(compose): add status config

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -2,7 +2,7 @@
 
 perform() {
     # shellcheck disable=SC2038
-    dirs=$(find . -mindepth 1 -maxdepth 1 -type d -not -name scripts -not -name bin -not -name grafana -not -name prometheus -not -name vault -not -path '*/\.*' | xargs -n 1 basename)
+    dirs=$(find . -mindepth 1 -maxdepth 1 -type d -not -name scripts -not -name bin -not -name grafana -not -name prometheus -not -name vault -not -name status -not -path '*/\.*' | xargs -n 1 basename)
 
     for dir in $dirs
     do
