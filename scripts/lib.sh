1c753d4cfb5821a8d77825a4ad538dd7798599a8
Alex Falkowski <alejandro.falkowski@wolt.com>
2025-06-12 12:29:07 +0200

    feat(root): add ability to build diff platforms

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2038,SC2155
 
-perform() {
-    # shellcheck disable=SC2038
-    dirs=$(find . -mindepth 1 -maxdepth 1 -type d -not -name scripts -not -name bin -not -name grafana -not -name prometheus -not -name vault -not -name status -not -path '*/\.*' | xargs -n 1 basename)
+function perform() {
+  local dirs=$(find . -mindepth 1 -maxdepth 1 -type d -not -name scripts -not -name bin -not -name grafana -not -name prometheus -not -name status -not -path '*/\.*' | xargs -n 1 basename)
 
-    for dir in $dirs
-    do
-      make -C "$dir" "$1"
-    done
+  for dir in $dirs; do
+    make -C "$dir" "$1"
+  done
 }
