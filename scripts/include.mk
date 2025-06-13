19c7b06d316c939313a1f82424955f23f1ded516
Alex Falkowski <alejandro.falkowski@wolt.com>
2025-06-13 09:05:07 +0300

    build(ci): use alexfalkowski/release:3.25 (#789)

--- scripts/include.mk
+++ scripts/include.mk
@@ -1,13 +1,21 @@
 # Build docker image.
 build-docker:
-	docker build -t alexfalkowski/$(IMAGE):$(VERSION).$(platform) .
+	docker build -t alexfalkowski/$(IMAGE):$(VERSION) .
 
 # Push built docker image.
 push-docker:
+	docker build -t alexfalkowski/$(IMAGE):$(VERSION) --push .
+
+# Build platform docker image.
+build-platform-docker:
+	docker build -t alexfalkowski/$(IMAGE):$(VERSION).$(platform) .
+
+# Push built platform docker image.
+push-platform-docker:
 	docker build -t alexfalkowski/$(IMAGE):$(VERSION).$(platform) --push .
 
-# Create a manifest.
-manifest-docker:
+# Create a platform manifest.
+manifest-platform-docker:
 	docker manifest create alexfalkowski/$(IMAGE):$(VERSION) --amend alexfalkowski/$(IMAGE):$(VERSION).amd64 --amend alexfalkowski/$(IMAGE):$(VERSION).arm64
 	docker manifest push alexfalkowski/$(IMAGE):$(VERSION)
 
